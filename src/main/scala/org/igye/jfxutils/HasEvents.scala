2d3068701c2ba9b05997c734db56b269039f096c
Igorocky <Igorocky@users.noreply.github.com>
2015-09-30 02:09:15 +0200

    Improve HasEvents.scala

--- src/main/scala/org/igye/jfxutils/HasEvents.scala
+++ src/main/scala/org/igye/jfxutils/HasEvents.scala
@@ -4,15 +4,22 @@ import javafx.event.{EventHandler, EventType, Event}
 import javafx.scene.Node
 
 class HasEvents(node: Node) {
-    def hnd[T <: Event](eventType: EventType[T])(hnd: T => Unit) {
+    def hnd[T <: Event](eventType: EventType[T])(hnd: T => Unit): HasEvents = {
         node.addEventHandler(eventType, new EventHandler[T] {
             override def handle(event: T): Unit = {
                 hnd(event)
             }
         })
+        this
     }
 
-    def hnd[T <: Event](eventHandlerInfo: EventHandlerInfo[T]) {
+    def hnd[T <: Event](eventHandlerInfo: EventHandlerInfo[T]): HasEvents = {
         node.addEventHandler(eventHandlerInfo.eventType, eventHandlerInfo.handler)
+        this
+    }
+
+    def hnd[T <: Event](eventHandlerInfos: EventHandlerInfo[T] *): HasEvents = {
+        eventHandlerInfos.foreach(info => node.addEventHandler(info.eventType, info.handler))
+        this
     }
 }
