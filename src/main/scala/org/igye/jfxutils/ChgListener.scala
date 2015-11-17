52cf80ee711ac6e111e61de8e1a0966f6987eb59
Igorocky <Igorocky@users.noreply.github.com>
2015-11-17 13:41:32 +0100

    Improve ChgListener

--- src/main/scala/org/igye/jfxutils/ChgListener.scala
+++ src/main/scala/org/igye/jfxutils/ChgListener.scala
@@ -2,13 +2,13 @@ package org.igye.jfxutils
 
 import javafx.beans.value.{ObservableValue, ChangeListener}
 
-case class OldNewValues[T](oldValue: T, newValue:T)
+case class OldNewValues[T](observable: ObservableValue[_ <: T], oldValue: T, newValue:T)
 
 object ChgListener {
     def apply[T](body: OldNewValues[T] => Unit): ChangeListener[T] = {
         new ChangeListener[T] {
             override def changed(observable: ObservableValue[_ <: T], oldValue: T, newValue: T): Unit = {
-                body(OldNewValues(oldValue, newValue))
+                body(OldNewValues(observable, oldValue, newValue))
             }
         }
     }
