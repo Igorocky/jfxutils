7b753333f686327c6383ebd36ea70ba6f4d202cd
Igorocky <Igorocky@users.noreply.github.com>
2015-11-20 01:39:26 +0100

    Improve <== operator

--- src/main/scala/org/igye/jfxutils/properties/PropertyOperators.scala
+++ src/main/scala/org/igye/jfxutils/properties/PropertyOperators.scala
@@ -3,9 +3,16 @@ package org.igye.jfxutils.properties
 import javafx.beans.property.Property
 import javafx.beans.value.ObservableValue
 
+import org.igye.jfxutils.propertyToPropertyOperators
+
 class PropertyOperators[T](property: Property[T]) {
     def <== (observableValue: ObservableValue[T]): Unit = {
         property.bind(observableValue)
+    }
+
+    def <== (anotherProperty: Property[T]): Property[T] = {
+        property <== anotherProperty.asInstanceOf[ObservableValue[T]]
+        anotherProperty
     }
 
     def <==> (anotherProperty: Property[T]): Unit = {
